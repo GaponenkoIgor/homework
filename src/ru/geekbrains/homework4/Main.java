643ed93d6d0af2b9062392f6d02b7ea0f365bf69
GaponenkoIgor <gaponenko.igor.aleksandrovich@yandex.ru>
2021-03-03 20:03:44 +0400

    first commit

---
+++ src/ru/geekbrains/homework4/Main.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.homework4;
+
+public class Main {
+
+    public static char[][] map;
+    public static final int SIze = 3;
+    public static final int DOT_
+
+
+    public static void main(String[] args) {
+
+    }
+}
